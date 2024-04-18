82b6063d6b50e761a7ec03a7ccb026b699384518
fickya1987 <89722540+fickya1987@users.noreply.github.com>
2024-04-18 14:00:37 +0700

    Update zomato.py

--- zomato.py
+++ zomato.py
@@ -7,7 +7,7 @@ data_cluster = pd.read_csv('data_cluster.csv', encoding='ISO-8859-1')
 
 
 # Streamlit app
-st.title("Restaurant Dashboard")
+st.title("Angkasa Pura 2 Airport Review Dashboard 02")
 
 # Filter cuisines
 selected_cuisines = st.multiselect("Select Cuisines", data_cluster.columns[8:52])
@@ -16,7 +16,7 @@ if selected_cuisines:
     filtered_restaurants = data_cluster[data_cluster.apply(lambda row: any(cuisine in row["Cuisines"] for cuisine in selected_cuisines), axis=1)]
 
     # Display top 5 restaurants based on average ratings
-    st.subheader("Top 5 Restaurants")
+    st.subheader("Top 5 Comments untuk Bandara AP2")
     top_restaurants = filtered_restaurants.sort_values(by="Rating", ascending=False).head(5)
     st.table(top_restaurants[["Name", "Rating", "Timings","Cuisines"]])
 
